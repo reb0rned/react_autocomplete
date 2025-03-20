dd9fafe764927cb5367bf0cc734fe771b7c6509a
reb0rned <affmarket77@gmail.com>
2025-03-20 18:45:47 +0200

    dropdown with debounce resolving

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,46 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 import './App.scss';
 import { peopleFromServer } from './data/people';
+import { Person } from './types/Person';
+import cn from 'classnames';
+import debounce from 'lodash.debounce'
 
 export const App: React.FC = () => {
-  const { name, born, died } = peopleFromServer[0];
+  const [isDroppedDown, setIsDroppedDown] = useState(false);
+  const [selectedPerson, setSelectedPerson] = useState<Person | null>(null);
+  const [query, setQuery] = useState('')
+  const [appliedQuery, setAppliedQuery] = useState('')
+
+  const applyQuery = useCallback(debounce(setAppliedQuery, 300), [])
+
+  const personClickHandler = (person: Person) => {
+    setIsDroppedDown(false)
+    setSelectedPerson(person)
+    setQuery(person.name)
+  }
+
+  const inputOnChangeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setQuery(event.target.value)
+    applyQuery(event.target.value)
+    setSelectedPerson(null)
+  }
+
+  const filteredPeople = peopleFromServer.filter(person => {
+    return person.name.toLowerCase().includes(appliedQuery.toLowerCase())
+  })
 
   return (
     <div className="container">
       <main className="section is-flex is-flex-direction-column">
-        <h1 className="title" data-cy="title">
-          {`${name} (${born} - ${died})`}
-        </h1>
+        {selectedPerson ? (
+          <h1 className="title" data-cy="title">
+          {`${selectedPerson.name} (${selectedPerson.born} - ${selectedPerson.died})`}
+          </h1>
+        ) : (
+          <h1 className="title" data-cy="title">
+            No selected person
+          </h1>
+        )}
 
         <div className="dropdown is-active">
           <div className="dropdown-trigger">
@@ -19,55 +49,54 @@ export const App: React.FC = () => {
               placeholder="Enter a part of the name"
               className="input"
               data-cy="search-input"
+              value={query}
+              onFocus={() => setIsDroppedDown(true)}
+              onChange={inputOnChangeHandler}
             />
           </div>
 
-          <div className="dropdown-menu" role="menu" data-cy="suggestions-list">
-            <div className="dropdown-content">
-              <div className="dropdown-item" data-cy="suggestion-item">
-                <p className="has-text-link">Pieter Haverbeke</p>
-              </div>
+          { isDroppedDown && (
+            <div className="dropdown-menu" role="menu" data-cy="suggestions-list">
+              <div className="dropdown-content">
 
-              <div className="dropdown-item" data-cy="suggestion-item">
-                <p className="has-text-link">Pieter Bernard Haverbeke</p>
-              </div>
+                {filteredPeople.map(person => (
+                  <div
+                  key={person.slug}
+                  className="dropdown-item"
+                  data-cy="suggestion-item"
+                  onClick={() => personClickHandler(person)}
+                  >
+                    <p
+                    className={cn({
+                      'has-text-link': person.sex === 'm',
+                      'has-text-danger': person.sex === 'f',
+                    })}
+                    >{person.name}</p>
+                  </div>
+                ))}
 
-              <div className="dropdown-item" data-cy="suggestion-item">
-                <p className="has-text-link">Pieter Antone Haverbeke</p>
-              </div>
-
-              <div className="dropdown-item" data-cy="suggestion-item">
-                <p className="has-text-danger">Elisabeth Haverbeke</p>
-              </div>
-
-              <div className="dropdown-item" data-cy="suggestion-item">
-                <p className="has-text-link">Pieter de Decker</p>
-              </div>
-
-              <div className="dropdown-item" data-cy="suggestion-item">
-                <p className="has-text-danger">Petronella de Decker</p>
-              </div>
-
-              <div className="dropdown-item" data-cy="suggestion-item">
-                <p className="has-text-danger">Elisabeth Hercke</p>
               </div>
             </div>
-          </div>
+          )}
         </div>
 
-        <div
-          className="
-            notification
-            is-danger
-            is-light
-            mt-3
-            is-align-self-flex-start
-          "
-          role="alert"
-          data-cy="no-suggestions-message"
-        >
-          <p className="has-text-danger">No matching suggestions</p>
-        </div>
+        {
+          filteredPeople.length === 0 && (
+            <div
+              className="
+                notification
+                is-danger
+                is-light
+                mt-3
+                is-align-self-flex-start
+              "
+              role="alert"
+              data-cy="no-suggestions-message"
+            >
+              <p className="has-text-danger">No matching suggestions</p>
+            </div>
+          )
+        }
       </main>
     </div>
   );
