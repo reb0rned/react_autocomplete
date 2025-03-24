9f9e6d61b2f61302de59ebed8772c06c6e946860
reb0rned <affmarket77@gmail.com>
2025-03-24 20:31:08 +0200

    search report bug resolving + additional block + lint warning fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,49 @@
-import React, { useCallback, useState } from 'react';
+import React, { useCallback, useMemo, useState } from 'react';
 import './App.scss';
 import { peopleFromServer } from './data/people';
 import { Person } from './types/Person';
-import cn from 'classnames';
-import debounce from 'lodash.debounce'
+import debounce from 'lodash.debounce';
+import { PeopleList } from './components/PeopleList';
 
 export const App: React.FC = () => {
   const [isDroppedDown, setIsDroppedDown] = useState(false);
   const [selectedPerson, setSelectedPerson] = useState<Person | null>(null);
-  const [query, setQuery] = useState('')
-  const [appliedQuery, setAppliedQuery] = useState('')
+  const [query, setQuery] = useState('');
+  const [appliedQuery, setAppliedQuery] = useState('');
 
-  const applyQuery = useCallback(debounce(setAppliedQuery, 300), [])
+  const debouncedSetQuery = useMemo(
+    () => debounce(setAppliedQuery, 300),
+    [setAppliedQuery],
+  );
+
+  const applyQuery = useCallback(debouncedSetQuery, [debouncedSetQuery]);
 
   const personClickHandler = (person: Person) => {
-    setIsDroppedDown(false)
-    setSelectedPerson(person)
-    setQuery(person.name)
-  }
+    setIsDroppedDown(false);
+    setSelectedPerson(person);
+    setQuery(person.name);
+  };
 
   const inputOnChangeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setQuery(event.target.value)
-    applyQuery(event.target.value)
-    setSelectedPerson(null)
-  }
+    setQuery(event.target.value);
+    applyQuery(event.target.value);
+    setSelectedPerson(null);
+  };
 
   const filteredPeople = peopleFromServer.filter(person => {
-    return person.name.toLowerCase().includes(appliedQuery.toLowerCase())
-  })
+    return person.name.toLowerCase().includes(appliedQuery.toLowerCase());
+  });
 
   return (
     <div className="container">
       <main className="section is-flex is-flex-direction-column">
-        {selectedPerson ? (
-          <h1 className="title" data-cy="title">
-          {`${selectedPerson.name} (${selectedPerson.born} - ${selectedPerson.died})`}
-          </h1>
-        ) : (
-          <h1 className="title" data-cy="title">
-            No selected person
-          </h1>
-        )}
+        <h1 className="title" data-cy="title">
+          {selectedPerson ? (
+            `${selectedPerson.name} (${selectedPerson.born} - ${selectedPerson.died})`
+          ) : (
+            `No selected person`
+          )}
+        </h1>
 
         <div className="dropdown is-active">
           <div className="dropdown-trigger">
@@ -51,52 +54,42 @@ export const App: React.FC = () => {
               data-cy="search-input"
               value={query}
               onFocus={() => setIsDroppedDown(true)}
+              onBlur={() => setIsDroppedDown(false)}
               onChange={inputOnChangeHandler}
             />
           </div>
 
-          { isDroppedDown && (
-            <div className="dropdown-menu" role="menu" data-cy="suggestions-list">
+          {isDroppedDown && filteredPeople.length > 0 && (
+            <div
+              className="dropdown-menu"
+              role="menu"
+              data-cy="suggestions-list"
+            >
               <div className="dropdown-content">
-
-                {filteredPeople.map(person => (
-                  <div
-                  key={person.slug}
-                  className="dropdown-item"
-                  data-cy="suggestion-item"
-                  onClick={() => personClickHandler(person)}
-                  >
-                    <p
-                    className={cn({
-                      'has-text-link': person.sex === 'm',
-                      'has-text-danger': person.sex === 'f',
-                    })}
-                    >{person.name}</p>
-                  </div>
-                ))}
-
+                <PeopleList
+                  filteredPeople={filteredPeople}
+                  personClickHandler={personClickHandler}
+                />
               </div>
             </div>
           )}
         </div>
 
-        {
-          filteredPeople.length === 0 && (
-            <div
-              className="
+        {filteredPeople.length === 0 && (
+          <div
+            className="
                 notification
                 is-danger
                 is-light
                 mt-3
                 is-align-self-flex-start
               "
-              role="alert"
-              data-cy="no-suggestions-message"
-            >
-              <p className="has-text-danger">No matching suggestions</p>
-            </div>
-          )
-        }
+            role="alert"
+            data-cy="no-suggestions-message"
+          >
+            <p className="has-text-danger">No matching suggestions</p>
+          </div>
+        )}
       </main>
     </div>
   );
